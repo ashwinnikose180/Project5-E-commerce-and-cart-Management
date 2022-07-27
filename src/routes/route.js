72b0386e1fc8a5da796da04b1f4bc6489b4e61ef
Mdsaif0786 <mdsaifrza11@gmail.com>
2022-07-27 17:14:28 +0530

    product create, filter, getById

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,15 +2,19 @@ const express= require("express")
 const router = express.Router();
 const userController = require("../controllers/userController")
 const {authenticate} = require('../middleware/auth')
+const productController = require("../controllers/productController")
 
-
-
+//===================== USER=========================================//
 
 router.post("/register", userController.createUser)
 router.post("/login", userController.userLogin)
 router.get("/user/:userId/profile" ,authenticate, userController.getUserProfile)
 router.put("/user/:userId/profile" , authenticate , userController.updateUserProfile)
 
+//=========================== PRODUCT=============================================//
 
+router.post("/products",productController.createProduct)
+router.get("/products",productController.filterProduct)
+router.get("/products/:productId",productController.getProductById)
 
 module.exports = router
