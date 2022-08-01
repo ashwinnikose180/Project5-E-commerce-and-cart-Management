08b798d09d1ef67b9dc16515b1154ce927770628
Amitmaz96 <amitm9088@gmail.com>
2022-08-01 16:24:52 +0530

    adding cart apis all

---
+++ src/models/cartModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const objectId = mongoose.Schema.Types.ObjectId
+
+const cartSchema = new mongoose.Schema({
+    userId : {type: objectId , ref: "user"},
+    items : {
+        type: [{productId : {type:objectId , ref: "product" , required: true} , quantity: {type: Number , required: true , default:1}}]
+    },
+    totalPrice : {type: Number , required: true},
+    totalItems: {type: Number , required: true},
+},{timestamps: true})
+
+module.exports = mongoose.model("cart" , cartSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController")
 const {authenticate} = require('../middleware/auth')
 const productController = require("../controllers/productController")
-
+const cartController = require("../controllers/cartController")
 
 //===================== USER=========================================//
 
@@ -20,6 +20,13 @@ router.get("/products/:productId",productController.getProductById)
 router.put("/products/:productId" , productController.updateProduct)
 router.delete("/products/:productId" , productController.deleteProduct)
 
+//=========================== CART=============================================//
+
+
+router.post("/users/:userId/cart" ,authenticate , cartController.createCart)
+router.put("/users/:userId/cart" , authenticate , cartController.updateCart)
+router.get("/users/:userId/cart" , authenticate , cartController.getCartDetails)
+router.delete("/users/:userId/cart" , authenticate , cartController.deletCart)
 
 router.all("/**" , (req , res)=>{
     res.status(400).send({message: "please enter correct endpoint"})
